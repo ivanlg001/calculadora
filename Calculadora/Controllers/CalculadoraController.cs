67c7dee3e45f5ca2fecd7743c6d3ab4004cdece5
Ivan Luevano <ILuevano@sigue.com>
2024-05-31 15:55:03 -0600

    Add project files.

---
+++ Calculadora/Controllers/CalculadoraController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Calculadora.Controllers
+{
+    public class CalculadoraController : ApiController
+    {
+        // GET: api/Calculadora
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Calculadora/5
+        public IHttpActionResult Get(int id)
+        {
+            string result = "entro";
+            if(result == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+        }
+
+        // POST: api/Calculadora
+        public IEnumerable<string> Post([FromBody]string value)
+        {
+            List<string> lstEcuacion = new List<string> { value };
+            return lstEcuacion;
+        }
+
+        // PUT: api/Calculadora/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Calculadora/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Calculadora/Scripts/calculadora.js
@@ -0,0 +1,84 @@
+﻿$(document).ready(function () { console.log("ready!!!"); });
+function btnUno() {
+    write("1");
+}
+function btnDos() {
+    write("2");
+}
+function btnTres() {
+    write("3");
+}
+function btnCuatro() {
+    write("4");
+}
+function btnCinco() {
+    write("5");
+}
+function btnSeis() {
+    write("6");
+}
+function btnSiete() {
+    write("7");
+}
+function btnOcho() {
+    write("8");
+}
+function btnNueve() {
+    write("9");
+}
+function btnCero() {
+    write("0");
+}
+function btnIgual() {
+    write("=");
+}
+function btnMas() {
+    write("+");
+}
+function btnMenos() {
+    write("1");
+}
+function btnSigno() {
+    write("-");
+}
+function btnX() {
+    write("x");
+}
+function btnY() {
+    write("y");
+}
+function btnClear() {
+    display.value = "";
+}
+function btnEnter() {
+    write("*** ");
+}
+let currentVal = "";
+function write(btnValue) {
+    
+    $('#display').val($('#display').val() + btnValue);
+    
+}
+
+function btnResolver() {
+    alert("Guardar");
+}
+
+
+function btnMemory() {
+    $.ajax({
+        type: "GET",
+        datatype: "json",
+        url: "api/Calculadora/" + 1,
+        success: function (result) {
+            alert(result);
+        },
+        error: function () {
+            alert("Error!!!");
+        }
+    });
+}
+
+
+
+
